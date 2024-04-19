d5bef337146e8878cc4e795d79e98770ac6f3e7b
Niu-Shuo <3259421483@qq.com>
2024-04-19 19:37:40 +0800

    Channel.h和Channel.cc提交

---
+++ reactor_thread_pool/Channel.cc
@@ -0,0 +1,79 @@
+#include "Channel.h"
+
+Channel::Channel(int fd, FDEvent events, handleFunc readFunc, handleFunc writeFunc, handleFunc destoryFunc, void *arg)
+{
+    // 类初始化
+    m_fd = fd;
+    // m_events = (int)events;
+    m_events = static_cast<int>(events);
+    m_arg = arg;
+
+    readCallback = readFunc;
+    writeCallback = writeFunc;
+    destroyCallback = destoryFunc;
+}
+
+/*
+    添加写属性
+    若对应为10 想要写添加写属性，与100异或，的110读写属性
+    如不写，第三位清零，若为110，第三位清零，将写取反011，在按位与& 010只留下读事件
+*/
+void Channel::writeEventEnable(bool flag)
+{
+    // 添加写事件
+    if (flag)
+    {
+        m_events |= static_cast<int>(FDEvent::WriteEvent); // WriteEvent 从右往左数第三个标志位1，通过异或 让channel->events的第三位为1
+    }
+    else
+    {
+        // ~WriteEvent 按位与， ~WriteEvent取反 011 然后与 channel->events按位与&运算 只有11 为 1，其它皆为0只有同为真时则真，一假则假，1为真，0为假
+        // m_events = m_events & ~static_cast<int>(FDEvent::WriteEvent);
+        m_events &= ~static_cast<int>(FDEvent::WriteEvent); // channel->events 第三位清零之后，写事件就不再检测
+    }
+}
+
+bool Channel::isWriteEventEnable()
+{
+    return m_events & static_cast<int>(FDEvent::WriteEvent); // 按位与 ，第三位都是1，则是写，如果成立，最后大于0，如果不成立，最后为0
+}
+
+void Channel::readEventEnable(bool flag)
+{
+    // 添加写事件
+    if (flag)
+    {
+        m_events |= static_cast<int>(FDEvent::ReadEvent); // ReadEvent 从右往左数第二个标志位1，通过异或 让channel->events的第二位为1
+    }
+    else
+    {
+        // ~ReadEvent 按位与， ~ReadEvent取反 011 然后与 channel->events按位与&运算 只有11 为 1，其它皆为0只有同为真时则真，一假则假，1为真，0为假
+        // m_events = m_events & ~static_cast<int>(FDEvent::ReadEvent);
+        m_events &= ~static_cast<int>(FDEvent::ReadEvent); // channel->events 第二位清零之后，写事件就不再检测
+    }
+}
+
+bool Channel::isReadEventEnable()
+{
+    return false;
+}
+
+void Channel::timeoutEventEnable(bool flag)
+{
+    // 添加写事件
+    if (flag)
+    {
+        m_events |= static_cast<int>(FDEvent::TimeOut); // TimeOut 从右往左数第一个标志位1，通过异或 让channel->events的第一位为1
+    }
+    else
+    {
+        // ~TimeOut 按位与， ~TimeOut取反 011 然后与 channel->events按位与&运算 只有11 为 1，其它皆为0只有同为真时则真，一假则假，1为真，0为假
+        // m_events = m_events & ~static_cast<int>(FDEvent::TimeOut);
+        m_events &= ~static_cast<int>(FDEvent::TimeOut); // channel->events 第一位清零之后，超时事件就不再检测
+    }
+}
+
+bool Channel::isTimeoutEventEnable()
+{
+    return false;
+}
---
+++ reactor_thread_pool/Channel.h
@@ -0,0 +1,82 @@
+#ifndef REACTOR_THREAD_POOL_CHANNEL_H
+#define REACTOR_THREAD_POOL_CHANNEL_H
+
+#include <functional>
+
+using handleFunc = std::function<int(void *)>;
+/*
+    参数泛型，handleFunc函数名 可以指向普通函数或者类里面的静态函数
+    using handleFunc = int(*)(void* );
+    定义文件描述符的读写事件，使用枚举   自定义
+*/
+
+//  C++11 强类型枚举
+enum class FDEvent
+{
+    TimeOut = 0x01,   // 十进制1，二进制001
+    ReadEvent = 0x02, // 十进制2，二进制010
+    WriteEvent = 0x04 // 十进制4，二进制100
+};
+
+/*
+    可调用对象包装器打包：函数指针，可调用对象(可以向函数一样使用)
+    最终得到了地址，但是没有调用
+*/
+
+// 描述符了文件描述符和读写，和读写回调函数以及参数
+class Channel
+{
+public:
+    // 构造函数
+    Channel(int fd, FDEvent events, handleFunc readFunc, handleFunc writeFunc, handleFunc destoryFunc, void *arg);
+
+    // 回调函数
+    handleFunc readCallback;    // 读回调
+    handleFunc writeCallback;   // 写回调
+    handleFunc destroyCallback; // 销毁回调
+
+    // 修改fd的写事件(检测 or 不检测)，flag是否为写事件
+    void writeEventEnable(bool flag);
+
+    // 判断的时候需要检测文件描述符的写事件
+    bool isWriteEventEnable();
+
+    // 修改fd的读事件(检测 or 不检测)，flag是否为读时间
+    void readEventEnable(bool flag);
+
+    // 判断的时候需要检测文件描述符的读事件
+    bool isReadEventEnable();
+
+    // 修改fd的销毁事件(检测 or 不检测)，flag是否为读时间
+    void timeoutEventEnable(bool flag);
+
+    // 判断的时候需要检测文件描述符的读事件
+    bool isTimeoutEventEnable();
+
+    /*
+        内联函数，函数调用不需要压栈，直接进行代码的替换，提高程序执行效率，但也需要更多内存，多用于简单
+
+    */
+
+    // 取出是由成员的值，使用此方法保证了安全性，可以直接读取，但不可修改，但地址仍可以修改 
+    // 取出事件
+    inline int getEvent(){
+        return m_events;
+    }
+
+    // 取出文件描述符
+    inline int getSocket(){
+        return m_fd;
+    }
+
+    // 取出回调函数参数
+    inline const void* getArg(){
+        return m_arg;
+    }
+private:
+    int m_fd;     // 文件描述符
+    int m_events; // 读/ 写/ 读写
+    void *m_arg;  // 回调函数参数
+};
+
+#endif
