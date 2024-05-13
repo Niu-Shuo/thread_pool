6c7cc6fedb2059b452cda8bf6007e7f3dfa7898b
Niu-Shuo <3259421483@qq.com>
2024-05-13 22:17:06 +0800

    buffer类相关函数修改

--- nrpc/net/buffer.cc
+++ nrpc/net/buffer.cc
@@ -5,60 +5,72 @@
 namespace nrpc
 {
     /*
-        find_crlf in read_buffer
-        -1  : not found
-        >=0 : found \r\n
+        find_crlf 在读缓冲区中
+        -1  : 没找到
+        >=0 : 找到了 \r\n
     */
     int Buffer::find_crlf(const size_t &len)
     {
         static const char *CRLF = "\r\n";
         static const size_t CRLFLen = 2;
 
+        // 用 memmem 函数在给定的缓冲区数据中查找 "\r\n"，如果找到则返回指向该位置的指针，否则返回 nullptr。
         void *ret = memmem(static_cast<const void *>(begin()), len, static_cast<const void *>(CRLF), CRLFLen);
         if (ret == nullptr)
         {
             return -1;
         }
+        // 返回其在缓冲区中的位置索引
         return static_cast<const char *>(ret) - begin();
     }
 
     /*
-        // write_buffer => socket
-        // -1 - EAGAIN
-        // 0  - EPIPE
-        // >0 - data size
-        // TODO data 和 extra_data 放到一起
+        将写缓冲区的数据写入到套接字中的功能，并返回写入的数据大小
+        -1 - EAGAIN
+        0  - EPIPE
+        >0 - 数据大小
+        TODO data 和 extra_data 放到一起
     */
     int Buffer::write_fd(int fd)
     {
+        // 初始化总共写入的数据大小
         int total = 0;
+        // 检查写缓冲区中是否有可读数据
         if (readable())
         {
+            // 使用系统调用 write 将可读数据写入到套接字中，返回写入的数据大小
             int n = ::write(fd, begin(), readable());
             if (n == -1)
             {
+                // 如果错误码是 EAGAIN、EWOULDBLOCK 或者 EINTR，表示应用程序可以稍后再试，此时返回 -1
                 if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR)
                 {
                     return -1;
                 }
+                // 如果错误码是 EPIPE，表示套接字的另一端已经关闭，此时返回 0
                 else if (errno == EPIPE)
                 {
                     return 0;
                 }
+                // 其他错误码表示写入发生错误，应用程序需要处理
                 else
                 {
                     PLOG_FATAL << "Buffer failed to invoke ::write";
                 }
             }
+            // 如果写入成功，更新读索引和累计写入数据大小
             else
             {
                 m_rindex += n;
                 total += n;
             }
         }
+        // 检查额外数据缓冲区中是否有数据可写入套接字
         if (extraable())
         {
+            // 使用系统调用 write 将额外数据写入到套接字中，返回写入的数据大小
             int n = ::write(fd, extra_begin(), extraable());
+            // 写入失败，处理方式同上
             if (n == -1)
             {
                 if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR)
@@ -74,45 +86,51 @@ namespace nrpc
                     PLOG_FATAL << "Buffer failed to invoke ::write";
                 }
             }
+            // 更新额外数据缓冲区的写索引和累计写入数据大小
             else
             {
                 m_extra_index += n;
                 total += n;
             }
         }
+        // 返回总共写入的数据大小
         return total;
     }
 
     /*
-        socket => read_buffer
+        从套接字中读取数据到写缓冲区的功能，并返回读取的数据大小
         -1 : EAGAIN
          0 : EOF
-        >0 : data size
-        TODO update ret value - EPIPE / ECONNRESET
+        >0 : 写入数据大小
+        TODO EPIPE / ECONNRESET情况的返回值更新
     */
     int Buffer::read_fd(int fd)
     {
+        // 检查写缓冲区是否有足够的空间用于写入数据，如果不够，则扩展写缓冲区大小
         if (writeable() == 0)
         {
             m_data.resize(m_data.size() * 2);
         }
+        // 系统调用 read 从套接字中读取数据，写入到写缓冲区中，返回读取的数据大小
         int n = ::read(fd, end(), writeable());
         if (n == -1)
         {
+            // 如果错误码是 EAGAIN、EWOULDBLOCK 或者 EINTR，表示应用程序可以稍后再试，此时返回 -1
             if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR)
             {
                 return -1;
             }
+            // 其他错误码表示读取发生错误，应用程序需要处理
             else
             {
                 PLOG_FATAL << "Buffer failed to invoke ::read";
             }
         }
+        // 更新写缓冲区的写索引，表示已经写入了多少数据
         else
         {
             m_windex += n;
         }
         return n;
-        return 0;
     }
 } // namespace nrpc
--- nrpc/net/buffer.h
+++ nrpc/net/buffer.h
@@ -1,5 +1,5 @@
-#ifndef NRPC_BASE_BUFFER_H
-#define NRPC_BASE_BUFFER_H
+#ifndef NRPC_NET_BUFFER_H
+#define NRPC_NET_BUFFER_H
 
 #include <string.h>
 #include <vector>
@@ -9,6 +9,18 @@
 
 namespace nrpc
 {
+    /*
+        Buffer 类：用于管理缓冲区的读写操作
+        1、提供了默认构造函数和带参数的构造函数用于初始化缓冲区，并在析构时释放资源。
+        2、状态成员函数：返回可读、可写取数据的字节数，返回读取区域和写入区域的指针，返回缓冲区的指针。
+        3、输入流成员函数：从缓冲区中读取 int32 和 uint32 类型的数据。
+        4、读取和写入成员函数：从缓冲区中读取指定字节数的数据，用于将数据从内存写入到文件描述符或从文件描述符读取到缓冲区。
+        5、追加数据：向缓冲区追加字符串或整数数据，追加额外的数据。
+        6、清空缓冲区
+
+        BufferView 类：用于创建缓冲区的视图，提供了访问缓冲区数据的接口。
+        重载了 operator<<，用于将 BufferView 中的数据流输出到流对象中
+    */
     class Buffer
     {
     public:
@@ -17,28 +29,28 @@ namespace nrpc
         virtual ~Buffer() {}
 
         // 状态成员函数
-        // 可读
+        // 可读字节数
         size_t readable() const
         {
             return m_windex - m_rindex;
         }
 
-        // 可写
+        // 可写字节数
         size_t writeable() const
         {
             return m_data.size() - m_windex;
         }
-
+        // 读取区域指针
         char *begin()
         {
             return &m_data[m_rindex];
         }
-
+        // 写入区域指针
         char *end()
         {
             return &m_data[m_windex];
         }
-
+        // 缓冲区指针
         const char *data() const
         {
             return &m_data[0];
@@ -55,14 +67,14 @@ namespace nrpc
             return &m_extra_data[m_extra_index];
         }
 
-        // 输入流成员函数
+        // 输入流成员函数-int32
         int32_t peek_int32()
         {
             int32_t *tmp = reinterpret_cast<int32_t *>(&m_data[m_rindex]);
             int32_t num = ntohl(*tmp);
             return num;
         }
-
+        // 输入流成员函数-uint32
         uint32_t peek_uint32()
         {
             uint32_t *tmp = reinterpret_cast<uint32_t *>(&m_data[m_rindex]);
@@ -73,87 +85,94 @@ namespace nrpc
         // find crlf
         int find_crlf(const size_t &len);
 
-        // read member functions
-        // retrieve data
+        // 读成员函数
+        // 从缓冲区中移除指定数量的字节数据
         void retrieve(size_t n)
         {
-            m_rindex += n;
+            m_rindex += n; // 将 m_rindex 更新为指向未读数据的新位置
         }
-        // mem => socket
+        // 将缓冲区中的数据写入到文件描述符（Socket）中
         int write_fd(int fd);
 
-        // write member functions
-        // socket => buffer
+        // 写成员函数
+        // 从文件描述符（Socket）中读取数据并写入到缓冲区中
         int read_fd(int fd);
 
-        // append
+        // 追加 char*
         void append(const char *str)
         {
             _append(str, strlen(str));
         }
-
+        // 追加 std::string
         void append(const std::string &str)
         {
             _append(str.data(), static_cast<int>(str.length()));
         }
-
+        // 追加 int32_t
         void append(int32_t num)
         {
             int32_t numm = htonl(num);
             _append(&numm, static_cast<int>(sizeof(int32_t)));
         }
-
+        // 追加 uint32_t
         void append(uint32_t num)
         {
             uint32_t numm = htonl(num);
             _append(&numm, static_cast<int>(sizeof(uint32_t)));
         }
 
-        // append_extra
+        // 接受一个常量引用 extra_data，将其内容复制到 m_extra_data 中。这种方式适用于传递常规的 std::string 类型的数据。
         void append_extra(const std::string &extra_data)
         {
             m_extra_data = extra_data;
         }
-
+        // 接受一个右值引用 extra_data，通过 std::move 将其内容移动到 m_extra_data 中，同时清空 extra_data。
         void append_extra(std::string &&extra_data)
         {
-            m_extra_data.swap(extra_data);
+            m_extra_data.swap(extra_data); // 隐式调用std::move()
         }
 
-        // clear
+        // 清空
         void clear()
         {
             m_rindex = m_windex = m_extra_index = 0;
         }
 
     private:
+        // 向缓冲区中追加数据
         void _append(const void *mem, int memlen)
         {
+            // 检查缓冲区中可写的空间是否足够容纳要追加的数据，如果不够，就需要扩展缓冲区的大小。
             if (static_cast<int>(writeable()) < memlen)
             {
                 m_data.resize(m_data.size() * 2);
             }
+            // 使用 memcpy 将 mem 中的数据复制到缓冲区中，从当前写入位置 m_windex 开始。
             memcpy(&m_data[m_windex], mem, memlen);
+            // 更新写入索引 m_windex，使其指向新追加的数据的末尾。
             m_windex += memlen;
         }
 
     public:
-        static const int InitialBufferSize = 1024;
+        static const int InitialBufferSize = 1024; // 初始化缓冲区的大小为1024
 
     protected:
-        size_t m_rindex, m_windex;
-        std::vector<char> m_data;
-        size_t m_extra_index;
-        std::string m_extra_data;
-        friend class BufferView;
+        size_t m_rindex, m_windex; // 读索引和写索引，用于标识缓冲区中可读和可写的位置
+        std::vector<char> m_data;  // 存储实际的数据，是缓冲区的主体
+        size_t m_extra_index;      // 标识额外数据的索引，表示额外数据的起始位置
+        std::string m_extra_data;  // 存储额外的数据，可能不会直接存储在缓冲区中，而是被存储在这个字符串中
+        friend class BufferView;   // 声明了BufferView类是Buffer类的友元类，这意味着BufferView类可以访问Buffer类的私有成员。
     };
 
     class BufferView
     {
     public:
         BufferView() : m_buf(nullptr), m_begin(0), m_end(0) {}
+        // 接受一个Buffer对象和一个长度参数。它将Buffer对象的地址存储在m_buf中，并使用Buffer对象的读索引作为起始位置，根据给定的长度计算结束位置
         BufferView(const Buffer &buf, size_t length) : m_buf(&buf), m_begin(m_buf->m_rindex), m_end(m_begin + length) {}
+        // 接受一个Buffer对象和两个索引参数，分别表示起始位置和结束位置
         BufferView(const Buffer &buf, size_t begin, size_t end) : m_buf(&buf), m_begin(begin), m_end(end) {}
+        // 接受一个Buffer对象、一个指向缓冲区数据的指针和一个长度参数。它计算起始位置，使得begin指针指向的位置成为缓冲区中的起始位置
         BufferView(const Buffer &buf, const char *begin, size_t length) : m_buf(&buf), m_begin(begin - m_buf->data()), m_end(m_begin + length) {}
 
         bool empty() const
@@ -161,6 +180,10 @@ namespace nrpc
             return m_begin == m_end;
         }
 
+        size_t size() const
+        {
+            return m_end - m_begin;
+        }
         std::string str() const
         {
             if (!m_buf)
@@ -171,10 +194,11 @@ namespace nrpc
         }
 
     private:
-        const Buffer *m_buf;
-        size_t m_begin, m_end;
+        const Buffer *m_buf;   // BufferView关联的缓冲区
+        size_t m_begin, m_end; // BufferView对象在缓冲区中的起始位置和结束位置
     };
 
+    // 重载 << ，将 BufferView 对象输出到流中
     inline std::ostream &operator<<(std::ostream &os, const BufferView &bufview)
     {
         os << bufview.str();
