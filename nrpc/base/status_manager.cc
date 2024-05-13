6aa96cef06637ac13ac6840dead5321344f7d09f
Niu-Shuo <3259421483@qq.com>
2024-05-13 22:20:11 +0800

    status_manager状态管理类函数修改及注释提交

--- nrpc/base/status_manager.cc
+++ nrpc/base/status_manager.cc
@@ -2,22 +2,26 @@
 
 namespace nrpc
 {
+    // 注册信号处理函数
     bool StatusManager::register_signals()
     {
         struct sigaction sa;
         sa.sa_flags = 0;
         sigemptyset(&sa.sa_mask);
         sigaddset(&sa.sa_mask, SIGINT);
-        sa.sa_handler = StatusManager::_handle_signal;
-        int err = sigaction(SIGINT, &sa, nullptr);
-        // PLOG_FATAL_IF(err == -1) << "StatusManager failed to invoke sigaction";
-        PLOG_FATAL_IF(err == -1) << "StatusManager failed to invoke sigaction"; 
+        sa.sa_handler = StatusManager::_handle_signal; // 设置信号处理函数为静态成员函数 _handle_signal
+        int err = sigaction(SIGINT, &sa, nullptr);     // 注册 SIGINT 信号的处理函数
+        // 如果注册失败，输出致命错误日志
+        PLOG_FATAL_IF(err == -1) << "StatusManager failed to invoke sigaction";
         return true;
     }
+
+    // 等待信号
     void StatusManager::wait_for_signal(int timeout_ms)
     {
-        std::unique_lock<std::mutex> ul(m_mutex);
+        std::unique_lock<std::mutex> ul(m_mutex); // 获取互斥锁
         // LOG_DEBUG << "StatusManager start to wait " << timeout_ms << " ms";
+        // 等待信号，直到超时或者被唤醒
         m_cv.wait_until(ul, std::chrono::milliseconds(timeout_ms) + std::chrono::system_clock::now());
     }
 } // namespace nrpc
--- nrpc/base/status_manager.h
+++ nrpc/base/status_manager.h
@@ -11,17 +11,25 @@
 
 namespace nrpc
 {
-    class StatusManager{
+    // 状态管理器类，用于管理程序的运行状态
+    class StatusManager
+    {
     public:
-        static StatusManager& get_instance() {
+        // 获取 StatusManager 的单例实例
+        static StatusManager &get_instance()
+        {
             static StatusManager sm;
             return sm;
         }
+
     private:
-        static void _handle_signal(int signo){
-            StatusManager& sm = StatusManager::get_instance();
-            sm._set_close();
+        // 处理信号的回调函数
+        static void _handle_signal(int signo)
+        {
+            StatusManager &sm = StatusManager::get_instance();
+            sm._set_close(); // 调用私有函数 _set_close() 设置程序关闭状态
         }
+
     public:
         // 注册信号
         bool register_signals();
@@ -30,28 +38,33 @@ namespace nrpc
         void wait_for_signal(int timeout_ms = 500);
 
         // 是否关闭
-        bool is_close() {
+        bool is_close()
+        {
             std::unique_lock<std::mutex> ul(m_mutex);
             return m_exit;
         }
+
     private:
-        // 设置关闭
-        void _set_close() {
+        // 设置程序关闭状态，并唤醒所有等待线程
+        void _set_close()
+        {
             std::unique_lock<std::mutex> ul(m_mutex);
             m_exit = true;
             m_cv.notify_all();
         }
-    protected:
-        StatusManager() : m_exit(false) {}
-        StatusManager(const StatusManager& taht) = delete;
-        StatusManager& operator=(const StatusManager& that) = delete;
-    private:
-        bool m_exit;
-        std::mutex m_mutex;
-        std::condition_variable m_cv;
 
+    protected:
+        // 构造函数，初始化成员变量
+        StatusManager() : m_exit(false) {}
+        // 禁止拷贝构造和赋值操作
+        StatusManager(const StatusManager &taht) = delete;
+        StatusManager &operator=(const StatusManager &that) = delete;
+
+    private:
+        bool m_exit;                  // 程序关闭标志
+        std::mutex m_mutex;           // 互斥锁，用于保护共享变量
+        std::condition_variable m_cv; // 条件变量，用于线程等待和唤醒
     };
 } // namespace nrpc
-
 
 #endif
